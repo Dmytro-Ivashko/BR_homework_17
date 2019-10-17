83a4375cfcb98e748a9b62de7dd88e84eda48b67
Dmytro Ivashko <ivashko.dmytro@gmail.com>
2019-10-17 20:21:21 +0300

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+
+function style() {
+    return gulp.src("./style/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./style"))
+}
+
+function watch() {
+    gulp.watch("./style/**/*.scss", style);
+}
+
+exports.style = style;
+exports.watch = watch;
