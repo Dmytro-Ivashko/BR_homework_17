a8ca538f021f5c441bc85e8b45445fed75e1e0c9
UKRoo <ivashko.dmytro@gmail.com>
2019-10-25 22:58:51 +0300

    fix css

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
-// const autoprefixer = require('gulp-autoprefixer');
+const cssbeautify = require('gulp-cssbeautify');
 
 function style() {
     return gulp.src("./style/**/*.scss")
-    .pipe(sass().on("error", sass.logError))
-    // .pipe(autoprefixer({}))
-    .pipe(gulp.dest("./style"))
+        .pipe(sass().on("error", sass.logError))
+        .pipe(cssbeautify())
+        .pipe(gulp.dest("./style"))
 }
+
 
 function watch() {
     gulp.watch("./style/**/*.scss", style);
